d515f0828838e116bf0a4df1fd3f3073f1c95c06
jayanthreddy1503 <jayanthreddy040315@gmail.com>
2026-09-25 14:27:31 +0530

    dockerfile and sever changes added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const os = require('os');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Serve static files (our simple frontend)
 app.use(express.static(path.join(__dirname, 'public')));
